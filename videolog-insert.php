1c76aae864983aaf278953b992dbc6a243af8b54
dulcetti <dulcetti@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-26 14:13:13 +0000

    Alteracao no tamanho padrao do video
    git-svn-id: https://plugins.svn.wordpress.org/videolog-insert-videos/trunk@337463 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- videolog-insert.php
+++ videolog-insert.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Videolog Insert Videos
 Version: 1.0
-Plugin URI: http://videolog.uol.com.br/blog/?p=458
+Plugin URI: http://www.videolog.tv/blog/?p=458
 Author: Videolog Technology Team
 Author URI: http://www.videolog.tv/blog/
 Description: Put videos of Portal Videolog.tv on WordPress with shortcodes
@@ -20,8 +20,8 @@ function videolog_callback($atts, $content=null) {
 	if ($height && !$width) $width = intval($height * 4 / 3);
 	if (!$height && $width) $height = intval($width * 3 / 4);
 	if (!$height && !$width) :
-		$width = 424;
-		$height = 318;
+		$width = 500;
+		$height = 375;
 	endif;
 
 	return	"<object width='$width' height='$height'>".
@@ -30,7 +30,7 @@ function videolog_callback($atts, $content=null) {
 			"<param name='movie' value='http://www.videolog.tv/ajax/codigoPlayer.php?id_video=$id_video&amp;relacionados=S&amp;default=S&amp;hd=S&amp;swf=1&amp;width=$width&amp;height=$height' />".
 			"<embed src='http://www.videolog.tv/ajax/codigoPlayer.php?id_video=$id_video&amp;relacionados=S&amp;default=S&amp;&hd=S&amp;swf=1&amp;width=$width&amp;height=$height' type='application/x-shockwave-flash' allowfullscreen='true' allowscriptaccess='always' width='$width' height='$height'></embed>".
 			"</object>".
-			"<br /><a href='http://www.videolog.tv/video.php?id=$id_video'>Veja o Video no Videolog.tv</a>.";
+			"<p><a href='http://www.videolog.tv/video?$id_video'>Veja o Video no Videolog.tv</a>.</p>";
 }
 
 add_shortcode('videolog', 'videolog_callback');
